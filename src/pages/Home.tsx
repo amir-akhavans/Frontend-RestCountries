ce63f91e654dfdeb9fafd663b846249782c8c976
Amir Akhavan <amir.akhavans@gmail.com>
2022-10-17 11:55:48 +0300

    CSS Minor Changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,31 +33,88 @@ export default function Home() {
     <>
       <Header />
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead >
-            <TableRow>
-              <TableCell sx={{paddingRight: 15, paddingLeft: 5}}>
+        <Table
+          sx={{
+            width: '100%',
+            minWidth: 650,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+          aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow
+              sx={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(6, 1fr)',
+              }}
+            >
+              <TableCell
+                sx={{
+                  gridColumn: '1 / 2',
+                }}
+              >
                 <h3>Flag</h3>
               </TableCell>
-              <TableCell sx={{paddingRight: 15}} >
+              <TableCell
+                sx={{
+                  gridColumn: '2 / 3',
+                }}
+              >
                 <h3>Name</h3>
               </TableCell>
-              <TableCell align="right" sx={{paddingRight: 25}} >
+              <TableCell
+                align="left"
+                sx={{
+                  gridColumn: '3 / 4',
+                }}
+              >
                 <h3>Languages</h3>
               </TableCell>
-              <TableCell align="right" sx={{paddingRight: 15}}>
+              <TableCell
+                align="left"
+                sx={{
+                  gridColumn: '4 / 5',
+                }}
+              >
                 <h3>Population</h3>
               </TableCell>
-              <TableCell align="right" sx={{paddingRight:15}} >
-            <h3>Region</h3>
-              </TableCell >{' '}
-              <TableCell align="right" sx={{paddingRight: 5}}></TableCell>
+              <TableCell
+                align="left"
+                sx={{
+                  gridColumn: '5 / 6',
+                }}
+              >
+                <h3>Region</h3>
+              </TableCell>{' '}
+              <TableCell
+                align="left"
+                sx={{
+                  gridColumn: '6 / 7',
+                }}
+              ></TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
+          <TableBody
+            sx={{
+              width: '100vw',
+            }}
+          >
             {countries.itemsToShow.map((country) => (
-              <TableRow key={country.name.common}>
-                <TableCell component="th" scope="row" >
+              <TableRow
+                key={country.name.common}
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(6, 1fr)',
+                }}
+              >
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{
+                    gridColumn: '1 / 2',
+                  }}
+                >
                   <TableRow key={country.name.common}>
                     <img
                       src={country.flags.png}
@@ -66,15 +123,44 @@ export default function Home() {
                     />
                   </TableRow>
                 </TableCell>
-                <TableCell component="th" scope="row" >
+                <TableCell
+                  component="th"
+                  scope="row"
+                  sx={{
+                    gridColumn: '2 / 3',
+                  }}
+                >
                   {country.name.common}
                 </TableCell>
-                <TableCell align="right"  sx={{paddingRight: 24}}>
+                <TableCell
+                  align="left"
+                  sx={{
+                    gridColumn: '3 / 4',
+                  }}
+                >
                   {Object.values(country.languages).map((lang) => lang)}
                 </TableCell>
-                <TableCell align="right" sx={{paddingRight: 15 }}>{country.population}</TableCell>
-                <TableCell align="right" sx={{paddingRight:15 }}>{country.region}</TableCell>
-                <TableCell  sx={{paddingRight:15 }}>
+                <TableCell
+                  align="left"
+                  sx={{
+                    gridColumn: '4 / 5',
+                  }}
+                >
+                  {country.population.toLocaleString()}
+                </TableCell>
+                <TableCell
+                  align="left"
+                  sx={{
+                    gridColumn: '5 / 6',
+                  }}
+                >
+                  {country.region}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    gridColumn: '6 / 7',
+                  }}
+                >
                   <Button
                     variant="contained"
                     color="success"
